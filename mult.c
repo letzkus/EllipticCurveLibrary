7a73d00cd5bb2d3c672ae1e1c7656277eb5403d9
Fabian Letzkus <letzkus@donotconnect.de>
2014-05-17 01:24:04 +0200

    Tried implementing R2L Mult. Algorithm..

--- mult.c
+++ mult.c
@@ -25,10 +25,10 @@
  *
  */
 int bp_lookup(uint32_t *a, uint32_t s, uint32_t *b, uint32_t *t) {
-	uint32_t lookupTable[16] = {0, 1, 4, 5,
-								16, 17, 20, 21
-								64, 65, 68, 69
-								80, 81, 84 85};
+	uint32_t lookupTable[16] = {0, 1, 4, 5, 16, 17, 20, 21 64, 65, 68, 69, 80, 81, 84 85};
+				
+	// TODO implement me :)
+								
 }
 
 /* 
@@ -52,9 +52,8 @@ int bp_lookup(uint32_t *a, uint32_t s, uint32_t *b, uint32_t *t) {
  *
  */
 void bp_mult(uint32_t m, uint32_t t, uint32_t *a, uint32_t *b) {
-	
-	uint32_t s[2*t];
-	
+		
+	// TODO implement me :)
 	
 }
 
@@ -65,7 +64,7 @@ void bp_mult(uint32_t m, uint32_t t, uint32_t *a, uint32_t *b) {
  * INPUT
  *    t - length t of array A
  *   *a - vector a
- *	 *b - vector b
+ *	  b - copy of vector b
  * 	  f - extension degree of binary field
 
  * OUTPUT
@@ -79,36 +78,27 @@ void bp_mult(uint32_t m, uint32_t t, uint32_t *a, uint32_t *b) {
  * 	 This implementation makes use of the R2L Shift and Add Algorithm for vectors
  *
  */
-void r2lvec_mult(uint32_t t, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *f) {
+void r2lvec_mult(uint32_t t, uint32_t *a, uint32_t b, uint32_t *c, uint32_t *f) {
+	
+	// f = x^m + r -> r = everything exept msb
+	uint32_t *r;
+	for(int i=0;i<t;i++) {
+		r[i] = f[i]
+	}
+	r[t-1] = r [t-1] && 127;
+	
 	for(int i=0;i<t;i++) {
 		for(int j=0;j<32;j++) {
 			
-			// Get a_i
-			uint32_t ai = (a[i] >> j) & 255;
-			
-			if(ai == 0) { // if a_i = 0 then
-				for(int k=0;k<t;k++) { // c = c ^ a
-					c[k] = c[k] ^ a[k]; 
-				}	
+			if((a[i] >> j) & 255 == 1) { // if a_i = 0 then
+				add(t,b,c,c)			 // 	c = c ^ a
 			}
 			
-			// b << 1
-			uint32_t sb[t];
-			shiftl(t,b,1,sb);
-			for(int k=0;k<t;k++) {
-				b[k] = sb[k];
-			}
-			
-			//Get b_m
-			// ... what is b_m???
-			uint32_t bm;
-			if(bm == 1) {
-				for(int k=0;k<t;k++) { // b = b ^ r
-					// Shouldn't that be c = b ^ r?????
-					
-				}
-			}
-			
+			shiftl(t,b,1,b);			// b << 1
+	
+			if(b[t-1] >> 7 == 1) {		// if b_m == 1 then
+				add(t,b,r,b)			//		b = b ^ r		
+			}			
 			
 		}
 	}
@@ -132,16 +122,6 @@ void r2lvec_mult(uint32_t t, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *f)
  *   - 
  */
 void test_mult() {
-	
-	uint32_t t = 6;
-	uint32_t m = 7;
-	
-	uint32_t a[t] = {0x00000002, 0x00000000, 0x00000000, 
-			0x00000000, 0x00000000, 0x00000000}; // 2 -> multInv(7, 2) = 3
-	uint32_t b[t] = {0x00000000, 0x00000000, 0x00000000, 
-			0x00000000, 0x00000000, 0x00000000}; // should be 3
-	uint32_t c[t] = {0x00000000, 0x00000000, 0x00000000, 
-			0x00000000, 0x00000000, 0x00000000}; // temporary...
 	
 	printf("\n************************************************************\n");
 	printf("test: multInv\n");
