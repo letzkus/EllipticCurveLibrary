c33f0f2cf45c7ccea9feae02a890309db3a5d93a
Fabian Letzkus <letzkus@donotconnect.de>
2014-05-16 17:29:40 +0200

    Added simple lookup table

--- mult.c
+++ mult.c
@@ -15,15 +15,19 @@
  *	 t - length of output
  *
  * RETURN 
- *   -
+ *   0 - everything ok
+ * 	 1 - key not found
  *
  * DESCRIPTION/REMARKS
- *   The function calculates a(x)*a(x) mod m within the binary field F_2^m
- * 	 This implementation makes use of the binary polynomquadration algorithm.
+ *   Makes a lookup to enable fast calculation of binary polynomquadration.
+ *	 Is currently fixed to 4 bit keys, so key t is useless right now...
  *
  */
 void bp_lookup(uint32_t *a, uint32_t *b, uint32_t *t) {
-	
+	uint32_t lookupTable[16] = {0, 1, 4, 5,
+								16, 17, 20, 21
+								64, 65, 68, 69
+								80, 81, 84 85};
 }
 
 /* 
