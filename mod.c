7df10de7b0beed9de813160ad4609a393a40d179
Fabian Letzkus <letzkus@donotconnect.de>
2014-05-19 10:50:08 +0200

    added functions for modulo operation

---
+++ mod.c
@@ -0,0 +1,30 @@
+/*
+ * Implementation of modulo operations
+ */
+
+/*
+ * FUNCTION
+ *  mod
+ *
+ * INPUT
+ *   length t of vector
+ *	 input vector a
+ *   modul vector f
+ *	 
+ *
+ * OUTPUT
+ *   result vector b
+ * 
+ * RETURN
+ *   -
+ * 
+ * DESCRIPTION/REMARKS
+ *   b = a mod f
+ * 
+ * EXAMPLE
+ * 	 
+ *
+ */
+void mod(uint32_t t, uint32_t *a, uint32_t *f, uint32_t *b) {
+	// b = a >> k for a = a[t]...a[0]
+}
