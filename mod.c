0694f4eb33f682077106bc751339a85e1b085168
Fabian Letzkus <letzkus@donotconnect.de>
2014-05-20 12:01:04 +0200

    added reduction by specified function f

--- mod.c
+++ mod.c
@@ -28,3 +28,33 @@
 void mod(uint32_t t, uint32_t *a, uint32_t *f, uint32_t *b) {
 	// b = a >> k for a = a[t]...a[0]
 }
+
+/*
+ * FUNCTION
+ *  mod_f163
+ *
+ * INPUT
+ *	 input vector a > f(x) as mentioned in description
+ *	 
+ * OUTPUT
+ *   output vector a reduced by f
+ * 
+ * RETURN
+ *   -
+ * 
+ * DESCRIPTION/REMARKS
+ *   Optimized for f(x) = x^163 + x^7 + x^6 + x^3 + 1
+ * 
+ */
+void mod_f163(uint32_t *c) {
+	for (int i=10;i>5;i--) {
+		uint32_t t = c[j];
+		c[j-6] = c[j-6] ^ (t << 29);
+		c[j-5] = c[j-5] ^ (t << 4) ^ (t << 3) ^ (t >> 3) ^ t;
+		c[j-4] = c[j-4] ^ (t >> 28) ^ (t >> 29)
+	}
+	t = (c[5] >> 3) << 3; // == c[5] & 0xFFFFFFF8
+	c[0] = c[0] ^ (t << 4) ^ (t << 3) ^ (t >> 3);
+	c[1] = c[1] ^ (t >> 28) ^ (t >> 29);
+	c[5] = c[5] & 0x7;
+}
